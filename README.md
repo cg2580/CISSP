d4faa779d27af918cfd2dc44681e762edaccde73
cg2580 <cg2580@yeah.net>
2024-06-24 07:06:48 +0800

    Updates

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CISSP
+
+## 安装依赖
+pip install -i "https://mirrors.aliyun.com/pypi/simple" streamlit==1.35.0
---
+++ pages/1_🌻_安全风险和管理.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="安全风险和管理", 
+    page_icon="☕", 
+    layout="wide",
+    initial_sidebar_state="auto", 
+    menu_items=None
+)
+
+st.header("网络安全治理")
+
+st.subheader("网络安全治理是个啥？")
+"""
+aaaa
+"""
+
+st.header("风险管理")
+
+
+st.header("合规")
+
+st.header("框架")
